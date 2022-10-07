Softmax fixed?

trecs/models/recommender.py
@@ -425,20 +425,17 @@ class BaseRecommender(MeasurementModule, SystemStateModule, VerboseMode, ABC):
         row = np.repeat(self.users.user_vector, item_indices.shape[1])
         row = row.reshape((self.num_users, -1))
         if self.probabilistic_recommendations:
-            permutation = s_filtered.argsort()
-            rec = item_indices[row, permutation]
             # the recommended items will not be exactly determined by
             # predicted score; instead, we will sample from the sorted list
             # such that higher-preference items get more probability mass
-            num_items_unseen = rec.shape[1]  # number of items unseen per user
             # use softmax rather than logspace to account for ties
             # NEEDS TO BE FIXED TO RESPECT ITEMS SORTING AND choice TO BE CALLED PER USER
             probabilities = np.exp(s_filtered - np.max(s_filtered, axis = 1, keepdims = True))
             probabilities = probabilities /  np.sum(probabilities, axis = 1, keepdims = True)
-            # probabilities = np.logspace(0.0, num_items_unseen / 10.0, num=num_items_unseen, base=2)
-            # probabilities = probabilities / probabilities.sum()
-            picks = np.random.choice(num_items_unseen, k, replace=False, p=probabilities)
-            return rec[:, picks]
+            picks = []
+            for u in range(self.num_users):
+            	picks.append(np.random.choice(item_indices[u], k, replace=False, p=probabilities[u])
+            return np.array(picks)
         else:
             # returns top k indices, sorted from greatest to smallest
             sort_top_k = mo.top_k_indices(s_filtered, k, self.random_state)