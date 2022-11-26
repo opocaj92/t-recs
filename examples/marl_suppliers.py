553198f31c5fd9fe6cadc007f30e6ed4f1517262
Jacopo <jacopo.castellini@gmail.com>
2022-11-26 12:57:45 +0100

    Single-agent RL env implemented

--- examples/marl_suppliers.py
+++ examples/marl_suppliers.py
@@ -1,10 +1,11 @@
 from stable_baselines3 import PPO
+import supersuit as ss
 import os
 
-from trecs.rl_envs import suppliers_env
+from trecs.rl_envs import ma_suppliers_parallel_env
 
 rec_type = "content_based"
-num_suppliers = 2
+num_suppliers = 5
 num_users = 100
 num_items = num_suppliers
 num_attributes = 100
@@ -14,7 +15,7 @@ simulation_steps = 100
 steps_between_training = 10
 max_preference_per_attribute = 5
 train_between_steps = True
-num_items_per_iter = 2
+num_items_per_iter = 3
 random_items_per_iter = 0
 repeated_items = True
 probabilistic_recommendations = False
@@ -23,15 +24,16 @@ all_items_identical = False
 price_into_observation = False
 rs_knows_prices = False
 
+num_envs = 4
 learning_rate = 0.0003
 gamma = 0.9999
 training_steps = 5000000
 DEBUG = True
 
-savepath = "Results/RLSuppliers"
+savepath = "Results/MARLSuppliers"
 os.makedirs(savepath, exist_ok = True)
 
-env = suppliers_env(
+env = ma_suppliers_parallel_env(
    rec_type = rec_type,
    num_suppliers = num_suppliers,
    num_users = num_users,
@@ -53,20 +55,25 @@ env = suppliers_env(
    rs_knows_prices = rs_knows_prices,
    savepath = savepath
 )
+env = ss.pad_observations_v0(env)
+env = ss.pad_action_space_v0(env)
+vec_env = ss.pettingzoo_env_to_vec_env_v1(env)
+vec_env = ss.concat_vec_envs_v1(vec_env, num_envs, num_cpus = 4, base_class = "stable_baselines3")
 
-model = PPO("MlpPolicy", env, learning_rate = learning_rate, gamma = gamma)
+model = PPO("MlpPolicy", vec_env, learning_rate = learning_rate, gamma = gamma)
 print("----------------- TRAINING -----------------")
 model.learn(total_timesteps = training_steps)
 model.save(savepath + "/suppliers_prices")
-env.render(mode = "training")
-env.close()
+vec_env.render(mode = "training")
+vec_env.close()
 
 print("-------------- TRAINING DONE ---------------")
-obs = env.reset()
-done = False
-while not done:
-   action = model.predict(obs, deterministic = True)[0]
-   obs, _, done, _ = env.step(action)
+observations = env.reset()
+env_done = False
+while not env_done:
+   actions = {agent: model.predict(observations[agent], deterministic = True)[0] for agent in env.agents}
+   observations, _, dones, _ = env.step(actions)
+   env_done = list(dones.values())[0]
 env.render(mode = "simulation")
 env.close()
 
@@ -86,3 +93,43 @@ if DEBUG:
       plt.savefig(savepath + "/Policy.pdf", bbox_inches = "tight")
       plt.clf()
       plt.close("all")
+
+   debug_savepath = os.path.join(savepath, "DEBUG")
+   os.makedirs(debug_savepath, exist_ok = True)
+
+   env = ma_suppliers_parallel_env(
+      rec_type = rec_type,
+      num_suppliers = num_suppliers,
+      num_users = num_users,
+      num_items = num_items,
+      num_attributes = num_attributes,
+      attention_exp = attention_exp,
+      pretraining = pretraining,
+      simulation_steps = simulation_steps,
+      steps_between_training = steps_between_training,
+      max_preference_per_attribute = max_preference_per_attribute,
+      train_between_steps = train_between_steps,
+      num_items_per_iter = num_items_per_iter,
+      random_items_per_iter = random_items_per_iter,
+      repeated_items = repeated_items,
+      probabilistic_recommendations = probabilistic_recommendations,
+      vertically_differentiate = vertically_differentiate,
+      all_items_identical = all_items_identical,
+      price_into_observation = price_into_observation,
+      rs_knows_prices = rs_knows_prices,
+      savepath = debug_savepath
+   )
+   env = ss.pad_observations_v0(env)
+   env = ss.pad_action_space_v0(env)
+
+   policies = np.choice([0., 0.25, 0.5, 0.75, 1.], size = (len(env.agents)))
+   repetitions = num_items if type(num_items) == list else [num_items // num_suppliers for _ in range(num_suppliers)]
+
+   _ = env.reset()
+   env_done = False
+   while not env_done:
+      actions = {agent: np.array([policies[a] for _ in range(repetitions[a])]) for a, agent in enumerate(env.agents)}
+      _, _, dones, _ = env.step(actions)
+      env_done = list(dones.values())[0]
+   env.render(mode = "simulation")
+   env.close()
--- trecs/rl_envs/__init__.py
+++ trecs/rl_envs/__init__.py
@@ -1,2 +1,3 @@
-from .suppliers import parallel_env as suppliers_parallel_env
-from .suppliers import env as suppliers_env
+from .ma_suppliers import parallel_env as ma_suppliers_parallel_env
+from .ma_suppliers import env as ma_suppliers_env
+from .rl_suppliers import env as suppliers_env
